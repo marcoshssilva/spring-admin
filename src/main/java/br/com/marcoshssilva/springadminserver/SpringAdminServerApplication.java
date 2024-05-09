c7fc6e51fcb05e951d08203efedb7efcd77985a0
marcoshssilva <76234459+marcoshssilva@users.noreply.github.com>
2024-05-08 22:16:50 -0300

    Setup project

--- src/main/java/br/com/marcoshssilva/springadminserver/SpringAdminServerApplication.java
+++ src/main/java/br/com/marcoshssilva/springadminserver/SpringAdminServerApplication.java
@@ -1,8 +1,16 @@
 package br.com.marcoshssilva.springadminserver;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@Configuration
+@EnableDiscoveryClient
+@EnableScheduling
+@EnableAdminServer
 @SpringBootApplication
 public class SpringAdminServerApplication {
 
