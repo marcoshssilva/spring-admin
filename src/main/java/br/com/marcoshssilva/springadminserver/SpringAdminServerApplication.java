6e6658ba52a6b9aaa8bd02cd1409ff1b4410baaa
marcoshssilva <76234459+marcoshssilva@users.noreply.github.com>
2024-05-08 21:44:09 -0300

    Startup project

---
+++ src/main/java/br/com/marcoshssilva/springadminserver/SpringAdminServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcoshssilva.springadminserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAdminServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/marcoshssilva/springadminserver/SpringAdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcoshssilva.springadminserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
